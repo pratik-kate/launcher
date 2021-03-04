fb22f050d8d0d299a7a08227ef392aa6268d7750
pratikkate3 <pratikrkate@gmail.com>
2021-03-04 19:28:28 +0530

    adding widgets

--- app/src/main/java/com/supertrident/launcher/HomeScreen.java
+++ app/src/main/java/com/supertrident/launcher/HomeScreen.java
@@ -1,17 +1,20 @@
 package com.supertrident.launcher;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class HomeScreen extends Fragment {
+public class HomeScreen extends Fragment implements View.OnLongClickListener {
 
     ImageView imageViewDrawer;
     public HomeScreen() {
@@ -22,6 +25,17 @@ public class HomeScreen extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_screen,container,false);
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT).show();
+                Intent pickIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_PICK);
+                pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 1);
+                startActivityForResult(pickIntent, 1);
+                return false;
+            }
+        });
 
         return view;
     }
@@ -47,18 +61,24 @@ public class HomeScreen extends Fragment {
 
     }
 
-    private boolean loadFragment(Fragment fragment) {
+    private void loadFragment(Fragment fragment) {
         //switching fragment
         if (fragment != null) {
             getActivity().getSupportFragmentManager()
                     .beginTransaction()
-                    .setCustomAnimations(android.R.anim.slide_in_left,
-                            android.R.anim.slide_out_right)
+                    .setCustomAnimations(android.R.anim.fade_in,
+                            android.R.anim.fade_out)
                     .replace(R.id.container, fragment)
                     .commit();
-            return true;
         }
-        return false;
     }
 
+    @Override
+    public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT).show();
+        Intent pickIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_PICK);
+        pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 1);
+        startActivityForResult(pickIntent, 1);
+        return false;
+    }
 }
