db496a36ec9cec351d9476604010756195872504
pratikkate3 <pratikrkate@gmail.com>
2021-03-05 15:20:00 +0530

    test

--- app/src/main/java/com/supertrident/launcher/MainActivity.java
+++ app/src/main/java/com/supertrident/launcher/MainActivity.java
@@ -2,34 +2,40 @@ package com.supertrident.launcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager2 viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(new HomeScreen());
+       // loadFragment(new HomeScreen());
+
+        viewPager = findViewById(R.id.myviewpager);
+        PagerAdapter myadapter = new PagerAdapter(getSupportFragmentManager());
+        
 
     }
 
-    private boolean loadFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.container, fragment)
-                    .commit();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onBackPressed() {
-        //super.onBackPressed();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeScreen()).commit();
-
-    }
+//    private boolean loadFragment(Fragment fragment) {
+//        if (fragment != null) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.container, fragment)
+//                    .commit();
+//            return true;
+//        }
+//        return false;
+//    }
+//    @Override
+//    public void onBackPressed() {
+//        //super.onBackPressed();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeScreen()).commit();
+//
+//    }
 }
